ce0a1ffec0a7ebbf0464ce0417cd379f4eeddf87
Wilk Maia <wilkmaia@gmail.com>
2018-10-11 13:22:11 -0300

    infrastructure: add helm to project
    Add Helm as the packaging and deploying solution for the project.
    
    Helm should be used to package and distribute the Kubernetes application
    and to deploy it to our cluster.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,16 +8,3 @@ module "gke" {
 
   cluster_name = "nthprime"
 }
-
-module "k8s" {
-  source   = "./k8s"
-  host     = "${module.gke.host}"
-  username = "${var.username}"
-  password = "${var.password}"
-
-  cluster_name = "nthprime"
-
-  client_certificate     = "${module.gke.client_certificate}"
-  client_key             = "${module.gke.client_key}"
-  cluster_ca_certificate = "${module.gke.cluster_ca_certificate}"
-}
